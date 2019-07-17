264deac5aa169dbd6e16bbc3c21bb263e29bf10b
Michel Carlos - micheldpcarlos <micheldpcarlos@gmail.com>
2019-07-17 02:39:29 -0300

    sports service and data manipulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreadcrumbConfig } from './models/breadcrumb.model';
 import { text } from '@fortawesome/fontawesome-svg-core';
+import { SportsService } from './services/sports.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  public breadcrumbConfig: BreadcrumbConfig = {
-    showIcon: true,
-    urlFragments: [
-      { fragment: 'page-name', text: 'Page Name' },
-      { fragment: 'breadcrumb', text: 'Breadcrumb' },
-      { fragment: 'current-page', text: 'Current Page' }
-    ]
+export class AppComponent implements OnInit{
+  public breadcrumbConfig = breadcrumbConfig;
+
+  constructor(private sportsService: SportsService) {
+
   }
+
+  ngOnInit() {
+    this.sportsService.getTableData().subscribe(res => console.log(res));
+  }
+}
+
+
+
+export const breadcrumbConfig : BreadcrumbConfig = {
+  showIcon: true,
+  urlFragments: [
+    { fragment: 'page-name', text: 'Page Name' },
+    { fragment: 'breadcrumb', text: 'Breadcrumb' },
+    { fragment: 'current-page', text: 'Current Page' }
+  ]
 }
---
+++ src/app/services/sports.service.ts
@@ -0,0 +1,113 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+// Models
+import { User } from '../models/user.model';
+import { Post } from '../models/post.model';
+import { Album } from '../models/album.model';
+import { Photo } from '../models/photo.model';
+import { CustomInfo } from '../models/custom-info.model';
+import { TableData } from '../models/table-data.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SportsService {
+
+  constructor(private http: HttpClient) { }
+
+  public getUsers() {
+    const url = environment.API_URL + '/users';
+    return this.http.get<User[]>(url)
+  }
+
+
+  public getPosts() {
+    const url = environment.API_URL + '/posts';
+    return this.http.get<Post[]>(url)
+  }
+
+
+  public getAlbums() {
+    const url = environment.API_URL + '/albums';
+    return this.http.get<Album[]>(url)
+  }
+
+
+  public getPhotos() {
+    const url = environment.API_URL + '/photos';
+    return this.http.get<Photo[]>(url)
+  }
+
+  public getCustomInfo() {
+    const url = environment.API_URL + '/custom-info';
+    return this.http.get<CustomInfo[]>(url)
+  }
+
+  public getTableData() {
+    const users = this.getUsers();
+    const posts = this.getPosts();
+    const albums = this.getAlbums();
+    const photos = this.getPhotos();
+    const customInfo = this.getCustomInfo();
+
+
+
+    return forkJoin([users, posts, albums, photos, customInfo]).pipe(map(result => {
+      const tableDataArray: TableData[] = [];
+      let tempTableData: TableData;
+
+      result[0].forEach(user => {
+        tempTableData = {
+          userId: user.id,
+          name: user.name,
+          username: user.username,
+          email: user.email,
+          address: user.address,
+          dayOfTheWeek: this.getDayOfTheWeek(user.id, result[4]),
+          rideInGroup: this.getRideInGroup(user.id, result[4]),
+          albums: this.countUserAlbums(user.id, result[2]),
+          posts: this.countUserPosts(user.id, result[1]),
+          photos: this.countUserPhotos(user.id, result[2], result[3])
+        }
+
+        tableDataArray.push(tempTableData)
+      })
+      return tableDataArray
+    }))
+  }
+
+  private countUserAlbums(userId: number, albums: Album[]): number {
+    // console.log(albums.filter(item => item.userId === userId))
+    return albums.filter(item => item.userId === userId).length
+  }
+
+  private countUserPosts(userId: number, posts: Post[]): number {
+    // console.log(posts.filter(item => item.userId === userId));
+    return posts.filter(item => item.userId === userId).length
+  }
+
+  private countUserPhotos(userId: number, albums: Album[], photos: Photo[]): number {
+    let photoCounter = 0;
+    const userAlbuns = albums.filter(item => item.userId === userId);
+
+    userAlbuns.forEach(album => {
+      photoCounter += photos.filter(photo => photo.albumId == album.id).length;
+    })
+
+    return photoCounter;
+  }
+
+  private getDayOfTheWeek(userId: number, customInfos: CustomInfo[]) {
+    return customInfos.find(item => item.userId === userId).dayOfTheWeek;
+  }
+
+  private getRideInGroup(userId: number, customInfos: CustomInfo[]) {
+    return customInfos.find(item => item.userId === userId).rideInGroup;
+  }
+}
